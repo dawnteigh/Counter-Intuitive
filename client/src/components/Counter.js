00056dfcbf56bef9a8ba92521241d19482c4c9d7
Donte Ladatto <donte279@gmail.com>
2023-08-14 22:03:42 -0500

    outside click hook

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useOutsideClick } from '../hooks/useOutsideClick'
 
 const Counter = () => {
 
@@ -11,16 +12,25 @@ const Counter = () => {
       setEdit(false)
     }
   }
+ 
+  const handleClick = (e) => {
+    setEdit(true)
+    e.stopPropagation()
+  }
+
+  const handleOutsideClick = () => setEdit(false)
+  
+  const ref = useOutsideClick(handleOutsideClick)
 
   return (
     <div className="counter">
       {
         edit ?
-          <div className="counter-name">
+          <div className="counter-name" ref={ref} >
             <input value={name} maxLength="25" onChange={(e) => setName(e.target.value)} onKeyDown={handleEnter} autoFocus />
           </div> :
           <div className="counter-name">
-            <span onClick={() => setEdit(true)}>{name}</span>
+            <span onClick={handleClick}>{name}</span>
           </div>
       }
       <div className="count">
