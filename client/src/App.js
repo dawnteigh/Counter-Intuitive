7bd552216e740748c9596cfc4cabaee6ea32b19c
Donte Ladatto <donte279@gmail.com>
2023-08-16 22:03:13 -0500

    controlled AddCounter form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Container from './components/Container'
+import AddCounter from './components/AddCounter';
 
 function App() {
   return (
     <div className="App">
       <Container />
+      <AddCounter />
     </div>
   );
 }
---
+++ client/src/components/AddCounter.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const AddCounter = () => {
+
+	const [formData, setFormData] = useState({
+		name: "",
+		class: "",
+		count: 0
+	})
+
+	const handleSelect = (e) => setFormData({...formData, class: e.target.value})
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value
+		})
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		// send form data to backend
+	}
+
+	return (
+		<div className="form-div">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Name of new counter:</label>
+				<input type="text" id="name" value={formData.name} onChange={handleChange} />
+				<label htmlFor="class" >Select a style:</label>
+				<select id="class" onChange={handleSelect}>
+					<option value="" >Default</option>
+					<option value="red" >Red</option>
+				</select>
+				<label htmlFor="name">Optional starting value (default is zero):</label>
+				<input type="number" id="count" value={formData.count} onChange={handleChange} />
+				<input type="submit" value="Add Counter" />
+      </form>
+		</div>
+	)
+}
+
+export default AddCounter
