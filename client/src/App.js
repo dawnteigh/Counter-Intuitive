7a95970db2025374a7b86c2eac6297f58f78c8cd
Donte Ladatto <donte279@gmail.com>
2023-08-23 22:38:05 -0500

    dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Container from './components/Container'
 import AddCounter from './components/AddCounter';
+import { useState } from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState("light")
+
+  const handleMode = () => {
+    if (mode === "light") setMode("dark")
+    else setMode("light")
+  }
+
   return (
-    <div className="App">
+    <div className={mode === "light" ? "App" : "App dark-mode"}>
+      <button id="dark-toggle" onClick={handleMode}>{mode === "light" ? "Dark Mode" : "Light Mode"}</button>
       <Container />
       <AddCounter />
     </div>
